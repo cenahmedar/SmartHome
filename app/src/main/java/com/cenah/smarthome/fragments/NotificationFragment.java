42c6df24b8acb53ffd0fb4cc82aaeececf104017
ahmed <cen.ahmedar@gmail.com>
2019-12-28 02:24:47 +0300

    notification

--- app/src/main/java/com/cenah/smarthome/fragments/NotificationFragment.java
+++ app/src/main/java/com/cenah/smarthome/fragments/NotificationFragment.java
@@ -1,15 +1,25 @@
 package com.cenah.smarthome.fragments;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cenah.smarthome.R;
+import com.cenah.smarthome.helpers.PrograssBarDialog;
+import com.cenah.smarthome.holders.NottificationViewHolder;
+import com.cenah.smarthome.models.Notification;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,16 +27,45 @@ import com.cenah.smarthome.R;
 public class NotificationFragment extends Fragment {
 
 
-    public NotificationFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerView recyclerView;
+    private PrograssBarDialog prograssBarDialog;
+    private View rootView;
+    private Activity activity;
 
+    private FirebaseRecyclerAdapter<Notification, NottificationViewHolder> adapter;
+    private DatabaseReference list;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        rootView = inflater.inflate(R.layout.fragment_notification, container, false);
+        activity = getActivity();
+
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(activity);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        list = db.getReference("Notification");
+        fetch();
+        return rootView;
+    }
+
+    private void fetch() {
+        adapter = new FirebaseRecyclerAdapter<Notification, NottificationViewHolder>
+                (Notification.class, R.layout.notificaiton_item, NottificationViewHolder.class,
+                        list) {
+            @Override
+            protected void populateViewHolder(NottificationViewHolder viewHolder, Notification model, int position) {
+                viewHolder.tx_desc.setText(model.getDescription().trim());
+                Picasso.get().load(model.getImage()).into(viewHolder.image);
+
+            }
+        };
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
     }
 
 }
